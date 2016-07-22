8d262ebb410f82f7f029a320040285a211aa3bbc
Tarik2121 <khorunziytaras@gmail.com>
2016-07-22 13:06:12 +0300

    Add files via upload

---
+++ taras/src/main/java/Match.java
@@ -0,0 +1,62 @@
+package hw_1;
+
+/**
+ * Created by Тарас on 22.07.2016.
+ */
+public class Match {
+    private String firstTeam;
+    private String secondTeam;
+    private int goalFirstTeam;
+    private int goalSecondTeam;
+
+    public Match() {
+    }
+
+    public Match(String firstTeam, String secondTeam, int goalFirstTeam, int goalSecondTeam) {
+        this.firstTeam = firstTeam;
+        this.secondTeam = secondTeam;
+        this.goalFirstTeam = goalFirstTeam;
+        this.goalSecondTeam = goalSecondTeam;
+    }
+
+    public String getFirstTeam() {
+        return firstTeam;
+    }
+
+    public void setFirstTeam(String firstTeam) {
+        this.firstTeam = firstTeam;
+
+    }
+
+    public String getSecondTeam() {
+        return secondTeam;
+    }
+
+    public void setSecondTeam(String secondTeam) {
+        this.secondTeam = secondTeam;
+
+    }
+
+    public int getGoalFirstTeam() {
+        return goalFirstTeam;
+    }
+
+    public void setGoalFirstTeam(int goalFirstTeam) {
+        this.goalFirstTeam = goalFirstTeam;
+
+    }
+
+    public int getGoalSecondTeam() {
+        return goalSecondTeam;
+    }
+
+    public void setGoalSecondTeam(int goalSecondTeam) {
+        this.goalSecondTeam = goalSecondTeam;
+
+    }
+
+    public String toString() {
+        return "Score: " + firstTeam + " " + goalFirstTeam + " - " + goalSecondTeam + " " + secondTeam;
+
+    }
+}
---
+++ taras/src/main/java/Task1.java
@@ -0,0 +1,12 @@
+package hw_1;
+
+/**
+ * Created by Тарас on 22.07.2016.
+ */
+public class Task1 {
+    public static void main(String[] args) {
+        Match match1 = new Match("Liverpool", "Everton", 4, 2);
+        System.out.println(match1);
+
+    }
+}
