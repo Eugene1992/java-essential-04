b4867181c68b0b569500f84e8a83a2834c9d3589
Eugene1992 <deyneko55@gmail.com>
2016-08-08 15:35:56 +0300

    Add reflex example.

---
+++ src/main/java/oop/reflex/Cat.java
@@ -0,0 +1,15 @@
+package oop.reflex;
+
+public class Cat {
+    public int publicField;
+    int packageField;
+    protected int protectedField;
+    private int privateField;
+    private final static int ID = 10;
+
+    public Cat() {
+    }
+
+    public void doSomething(){
+    }
+}
